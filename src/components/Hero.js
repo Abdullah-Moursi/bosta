c16e315f8740a5b3b905cb07347acd5b8833c37b
Abdullah-Moursi <moursiabdullah@gmail.com>
2021-10-30 00:28:26 +0200

    css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container, Row, Col } from "react-bootstrap";
 import Button from "@mui/material/Button";
 import header from "assets/images/header.jpg";
 
@@ -11,56 +11,105 @@ const Hero = () => {
         <Card.ImgOverlay
           style={{
             padding: "4% 20%",
-            textAlign: "right",
           }}
         >
-          <Card.Title
-            style={{
-              fontSize: "190%",
-              fontWeight: "700",
-            }}
-          >
-            مع بوسطة, اشحن مكان ما تحب
-          </Card.Title>
-          <Card.Text
-            style={{
-              fontSize: "109%",
-            }}
-          >
-            وصّل منتجاتك أسرع وأوفر مع بوسطة لأي مكان في مصر بأقل
-            <br />
-            سعر ومجهود وتابع تحركات الشحن والمندوب أول بأول من غير
-            <br />
-            تأخير ولا تعب في الفواتير
-          </Card.Text>
+          <Container>
+            <Row>
+              <Col xs={{ order: 1 }}>
+                <h1
+                  style={{
+                    fontSize: "190%",
+                    fontWeight: "700",
+                  }}
+                >
+                  مع بوسطة, اشحن مكان ما تحب
+                </h1>{" "}
+                <p
+                  style={{
+                    fontSize: "109%",
+                  }}
+                >
+                  وصّل منتجاتك أسرع وأوفر مع بوسطة لأي مكان في مصر بأقل
+                  <br /> سعر ومجهود وتابع تحركات الشحن والمندوب أول بأول من غير
+                  <br />
+                  تأخير ولا تعب في الفواتير
+                </p>{" "}
+                <div
+                  style={{
+                    marginTop: "1.5%",
+                  }}
+                >
+                  <Button
+                    href="https://bosta.co/contact-shipping-sales/?lang=ar"
+                    id="homeButtons"
+                    style={{
+                      borderStyle: "solid",
+                      borderWidth: "2px",
+                      backgroundColor: "transparent",
+                      borderColor: "white",
+                    }}
+                  >
+                    كلم المبيعات
+                  </Button>
+                  <Button
+                    style={{
+                      backgroundColor: "red",
+                    }}
+                    href="https://business.bosta.co/signup"
+                    id="homeButtons"
+                  >
+                    ابدأ مع بوسطـة
+                  </Button>
+                </div>{" "}
+              </Col>
+            </Row>
 
-          <div
-            style={{
-              marginTop: "3.5%",
-            }}
-          >
-            <Button
-              href="https://bosta.co/contact-shipping-sales/?lang=ar"
-              id="homeButtons"
-              style={{
-                borderStyle: "solid",
-                borderWidth: "2px",
-                backgroundColor: "transparent",
-                borderColor: "white",
-              }}
-            >
-              كلم المبيعات
-            </Button>
-            <Button
-              style={{
-                backgroundColor: "red",
-              }}
-              href="https://business.bosta.co/signup"
-              id="homeButtons"
-            >
-              ابدأ مع بوسطـة
-            </Button>{" "}
-          </div>
+            {/* <Row>
+              <Col xs={{ order: 1 }}>
+                <p
+                  style={{
+                    fontSize: "109%",
+                  }}
+                >
+                  وصّل منتجاتك أسرع وأوفر مع بوسطة لأي مكان في مصر بأقل سعر
+                  ومجهود وتابع تحركات الشحن والمندوب أول بأول من غير تأخير ولا
+                  تعب في الفواتير
+                </p>{" "}
+              </Col>
+            </Row>
+
+            <Row>
+              <Col xs={{ order: 1 }}>
+                <div
+                  style={{
+                    marginTop: "1.5%",
+                  }}
+                >
+                  <Button
+                    href="https://bosta.co/contact-shipping-sales/?lang=ar"
+                    id="homeButtons"
+                    style={{
+                      borderStyle: "solid",
+                      borderWidth: "2px",
+                      backgroundColor: "transparent",
+                      borderColor: "white",
+                    }}
+                  >
+                    كلم المبيعات
+                  </Button>
+                  <Button
+                    style={{
+                      backgroundColor: "red",
+                    }}
+                    href="https://business.bosta.co/signup"
+                    id="homeButtons"
+                  >
+                    ابدأ مع بوسطـة
+                  </Button>
+                </div>{" "}
+              </Col>
+            </Row> */}
+          </Container>
         </Card.ImgOverlay>
       </Card>
     </div>
